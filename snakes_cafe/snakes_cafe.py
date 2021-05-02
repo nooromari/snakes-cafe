f1586c2b30d033751d4467c48f2448121ca431da
NOOR OMARI <nooromari847@gmail.com>
2021-05-02 18:38:26 +0300

    lab01

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,52 @@
+hello = """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+"""
+print(hello)
+
+num_order = []
+order = 'order'
+user_order= ""
+while user_order != "quit":
+   user_order= input('> ')
+   num_order.append(user_order)
+   print()
+   if user_order == "quit":
+       break
+   if num_order.count(user_order) > 1:
+
+   print(f"** {num_order.count(user_order)} order of {user_order} have been added to your meal **")
+   print()
